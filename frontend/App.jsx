1c68c9e095b5f39e3443c0d112b47b1c939e6a53
Vani21-ps <jainvani132020@gmail.com>
2026-03-26 20:24:39 +0530

    rollback to vite

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -27,7 +27,7 @@ export default function App() {
 
   const [editingId, setEditingId] = useState(null);
 
-  const API = process.env.REACT_APP_API;
+const API = import.meta.env.VITE_API;
 
   const fetchContacts = async () => {
     const res = await fetch(API);
